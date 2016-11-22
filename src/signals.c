0df82d1cb0ad3532625455e8955c70de2207d785
Brett Sheffield <brett@gladserv.com>
2016-11-22 13:22:18 +0000

    added skeleton signal handlers

--- src/signals.c
+++ src/signals.c
@@ -1,7 +1,29 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "errors.h"
 #include "signals.h"
 
 int sighandlers()
 {
-	return ERROR_FAIL;
+	signal(SIGHUP, sighup_handler);
+	signal(SIGINT, sigint_handler);
+	signal(SIGTERM, sigterm_handler);
+
+	return ERROR_SUCCESS;
+}
+
+void sighup_handler (int signo)
+{
+	// do nothing, yet
+}
+
+void sigint_handler (int signo)
+{
+	sigterm_handler(signo);
+}
+
+void sigterm_handler (int signo)
+{
+	_exit(EXIT_SUCCESS);
 }
--- src/signals.h
+++ src/signals.h
@@ -2,5 +2,8 @@
 #define __LIBRECAST_SIGNALS_H__ 1
 
 int sighandlers();
+void sighup_handler (int signo);
+void sigint_handler (int signo);
+void sigterm_handler (int signo);
 
 #endif /* __LIBRECAST_SIGNALS_H__ */
