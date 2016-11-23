6b9d3a15ef2739d60e5ed41e934f8144557c6a25
Brett Sheffield <brett@gladserv.com>
2016-11-23 20:56:35 +0000

    arg handling

--- src/args.h
+++ src/args.h
@@ -2,12 +2,13 @@
 #define __LIBRECAST_ARGS_H__ 1
 
 #define ARGS_ARGS(X) \
-	X("start", "start the daemon") \
-	X("stop", "stop the daemon") \
-	X("status", "report status of daemon")
+	X("start", "start the daemon", 0) \
+	X("stop", "stop the daemon", 0) \
+	X("status", "report status of daemon", 0)
 #undef X
 
-#define ARGS_ARG(name, desc) if (strcmp(arg, name) == 0) return 1;
+#define ARGS_ARG(name, desc, f) if (strcmp(arg, name) == 0) return 1;
+#define ARGS_FUNC(name, desc, f) if (strcmp(arg, name) == 0) return f;
 
 /* process all command line args and options */
 int args_process(int argc, char **argv);
