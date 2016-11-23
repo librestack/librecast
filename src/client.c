89e188067b5b46dcf1876bb33d8c3de4336bbe6d
Brett Sheffield <brett@gladserv.com>
2016-11-23 22:02:16 +0000

    client stop

--- src/client.c
+++ src/client.c
@@ -1,7 +1,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 #include "main.h"
+#include "config.h"
 #include "errors.h"
+#include "args.h"
 #include "log.h"
 #include "pid.h"
 #include "signals.h"
@@ -11,12 +14,31 @@ int main(int argc, char **argv)
 	int e, errsv;
 	int lockfd;
 
+	config_set_num("loglevel", LOG_DEBUG);
+
+	if ((e = args_process(argc, argv)) != 0) {
+		goto main_fail;
+	}
+
 	lockfd = obtain_lockfile(O_RDONLY);
 	if (lockfd == -1) {
 		errno = 0;
 		e = ERROR_PID_OPEN;
 		goto main_fail;
 	}
+
+	if (argc != 2) {
+		e = ERROR_INVALID_ARGS;
+		goto main_fail;
+	}
+
+	if (!args_valid_arg(argv[1])) {
+		e = ERROR_INVALID_ARGS;
+		goto main_fail;
+	}
+
+	if (strcmp(argv[1], "stop") == 0)
+		signal_daemon(SIGINT, lockfd);
 
 main_fail:
 	errsv = errno;
