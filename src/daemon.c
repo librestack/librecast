0c2f572c4ad31e68ad61c303f14c74e5a6195783
Brett Sheffield <brett@gladserv.com>
2016-11-24 07:21:33 +0000

    removing superfluous includes

--- src/daemon.c
+++ src/daemon.c
@@ -1,13 +1,7 @@
 #define _GNU_SOURCE
 
 #include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/file.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include "config.h"
 #include "main.h"
