7743b64e9dd363e797aac66e318b3299e6e9ee63
Brett Sheffield <brett@gladserv.com>
2020-07-23 19:43:32 +0100

    tests for lc_socket_listen() / lc_socket_listen_cancel()
    check socket is not null in lc_socket_listen()

--- src/librecast.c
+++ src/librecast.c
@@ -1202,6 +1202,8 @@ int lc_socket_listen(lc_socket_t *sock, void (*callback_msg)(lc_message_t*),
 	pthread_attr_t attr = {};
 	lc_socket_call_t *sc;
 
+	if (sock == NULL) return lc_error_log(LOG_DEBUG, LC_ERROR_SOCKET_REQUIRED);
+
 	sc = calloc(1, sizeof(lc_socket_call_t));
 	sc->sock = sock;
 	sc->callback_msg = callback_msg;
---
+++ test/0000-0011.c
@@ -0,0 +1,52 @@
+#include "test.h"
+#include "../src/errors.h"
+
+void callback_recv(lc_message_t *msg)
+{
+}
+
+void callback_err(int err)
+{
+}
+
+int main()
+{
+	result("lc_socket_listen() / lc_socket_listen_cancel()");
+	lc_ctx_t *lctx = NULL;
+	lc_socket_t *sock = NULL; 
+	lc_channel_t *chan = NULL;
+
+	lctx = lc_ctx_new();
+	sock = lc_socket_new(lctx);
+	chan = lc_channel_new(lctx, "example.com");
+
+	lc_channel_bind(sock, chan);
+	lc_channel_join(chan);
+
+	test_assert(lc_socket_listen(NULL, NULL, NULL) == LC_ERROR_SOCKET_REQUIRED,
+			"lc_socket_listen requires socket");
+
+	test_assert(lc_socket_listen(sock, NULL, NULL) == 0,
+			"lc_socket_listen() returns 0 on success");
+
+	test_assert(lc_socket_listen(sock, NULL, NULL) == LC_ERROR_SOCKET_LISTENING,
+			"lc_socket_listen() returns LC_ERROR_SOCKET_LISTENING when socket busy");
+
+	test_assert(lc_socket_listen_cancel(sock) == 0,
+			"lc_socket_listen_cancel() returns 0 on success");
+
+	test_assert(lc_socket_listen_cancel(sock) == 0,
+			"lc_socket_listen_cancel() can be called twice");
+
+	test_assert(lc_socket_listen(sock, callback_recv, callback_err) == 0,
+			"lc_socket_listen() returns 0 on success");
+
+	test_assert(lc_socket_listen_cancel(sock) == 0,
+			"lc_socket_listen_cancel() cleans up memory");
+
+	lc_channel_free(chan);
+	lc_socket_close(sock);
+	lc_ctx_free(lctx);
+
+	return fails;
+}
