b78b6778b4d28292bc907bb2b79d91c8ab3b0f7c
Brett Sheffield <brett@gladserv.com>
2020-08-09 14:41:02 +0100

    add timeout to lc_msg_send() / lc_msg_recv() test

--- test/0000-0019.c
+++ test/0000-0019.c
@@ -2,15 +2,33 @@
 #include <librecast/net.h>
 #include "../src/errors.h"
 #include "../src/log.h"
+#include <errno.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+void sighandler(int sig)
+{
+	/* do nothing => SIG_IGN ? */
+}
+
+void *testthread(void *arg)
+{
+	struct timespec ts = { 0, 200 };
+	nanosleep(&ts, NULL);
+	kill(getpid(), SIGINT);
+	pthread_exit(arg);
+}
 
 int main()
 {
 	test_name("lc_msg_send() / lc_msg_recv() - blocking network recv");
 	LOG_LEVEL = 127;
 
+	struct sigaction sa = { .sa_handler = sighandler };
+	pthread_t thread;
+	pthread_attr_t attr = {};
 	char channame[] = "example.com";
 	char data[] = "black lives matter";
 	const int on = 1;
@@ -20,6 +38,13 @@ int main()
 	lc_socket_t *sock = lc_socket_new(lctx);
 	lc_channel_t *chan = lc_channel_new(lctx, channame);
 	lc_message_t msg;
+
+	/* set up signal handler so we can kill recv() */
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+
+	/* fire up test thread */
+	pthread_attr_init(&attr);
 
 	/* talking to ourselves, set loopback */
 	test_assert(!lc_socket_setopt(sock, IPV6_MULTICAST_LOOP, &on, sizeof(on)),
@@ -33,7 +58,12 @@ int main()
 	lc_msg_set(&msg, LC_ATTR_OPCODE, &op);
 	byt_sent = lc_msg_send(chan, &msg);
 	lc_msg_free(&msg);			/* clear struct before recv */
+
+	pthread_create(&thread, &attr, testthread, NULL);
 	byt_recv = lc_msg_recv(sock, &msg);	/* blocking recv */
+	pthread_cancel(thread);
+	pthread_join(thread, NULL);
+
 	test_log("sent %zi bytes", byt_sent);
 	test_log("recv %zi bytes", byt_recv);
 	test_assert(byt_sent == byt_recv, "bytes sent == bytes received (1)");
@@ -42,13 +72,20 @@ int main()
 	lc_msg_init_data(&msg, &data, strlen(data + 1), NULL, NULL);
 	byt_sent = lc_msg_send(chan, &msg);
 	lc_msg_init(&msg);
+
+	pthread_create(&thread, &attr, testthread, NULL);
+	errno = 0;
 	byt_recv = lc_msg_recv(sock, &msg);	/* blocking recv */
+	pthread_cancel(thread);
+	pthread_join(thread, NULL);
+
+	test_assert(errno != EINTR, "lc_msg_recv EINTR");
 	test_log("sent %zi bytes", byt_sent);
 	test_log("recv %zi bytes", byt_recv);
 	test_assert(byt_sent == byt_recv, "bytes sent == bytes received (2)");
 	test_expectn(data, msg.data, msg.len); /* got our data back */
 	lc_msg_free(&msg);
-	
+
 	lc_channel_free(chan);
 	lc_socket_close(sock);
 	lc_ctx_free(lctx);
